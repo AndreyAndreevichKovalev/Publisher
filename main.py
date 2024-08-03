b9124cabe319dc30af7a99b61adcf581410a337d
Andrey Kovalev <world_classic@mail.ru>
2024-08-04 02:46:42 +0900

    Second commit

--- main.py
+++ main.py
@@ -8,21 +8,6 @@ from dotenv import load_dotenv
 
 Base = declarative_base()
 
-class Course(Base):
-    __tablename__ = "course"
-
-    id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(length=40), unique=True)
-
-class Homework(Base):
-    __tablename__ = "homework"
-
-    id = sq.Column(sq.Integer, primary_key=True)
-    number = sq.Column(sq.Integer, nullable=False)
-    description = sq.Column(sq.Text, nullable=False)
-    course_id = sq.Column(sq.Integer, sq.ForeignKey("course.id"), nullable=False)
-
-    course = relationship(Course, backref="homeworks")
 
 class Publisher(Base):
     __tablename__ = "publisher"
